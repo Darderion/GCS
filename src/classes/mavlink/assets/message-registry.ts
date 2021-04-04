80a8a93195d0af3ab616617270d6076fcfdfb4e5
Darderion <Eobard1SA@gmail.com>
2021-04-05 02:54:32 +0300

    Changed generated ts imports

--- src/classes/mavlink/assets/message-registry.ts
+++ src/classes/mavlink/assets/message-registry.ts
@@ -1,4 +1,4 @@
-import {MAVLinkMessage} from 'node-mavlink';
+import {MAVLinkMessage} from '@ifrunistuttgart/node-mavlink';
 import {Heartbeat} from './messages/heartbeat';
 import {ProtocolVersion} from './messages/protocol-version';
 export const messageRegistry: Array<[number, new (system_id: number, component_id: number) => MAVLinkMessage]> = [
